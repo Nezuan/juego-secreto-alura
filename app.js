0433ab99e08b1bb630be7d811ea5994f88cdeb9a
Nezuan <juan.m.funez@hotmail.com>
2024-06-26 19:17:09 -0300

    Versión final de juego de adivinar número

---
+++ app.js
@@ -0,0 +1,71 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+console.log(numeroSecreto)
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos == 1)? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute("disabled");
+    } else {
+        //El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p',' El número secreto es menor');
+        }else{
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja()
+    }
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+   
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p',`Indique un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNúmeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    //limpiar caja;
+    limpiarCaja();
+    //indicar mensaje de intervalo de números
+    //generear el número aleatorio 
+    //inicializar el contador de intentos
+    condicionesIniciales();
+    //deshabilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled',true)
+}
+
+
+function generarNúmeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    //si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles')
+    }else
+        //si el número generado esta incluido en la lista
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return numeroGenerado
+        }else{
+            listaNumerosSorteados.push(numeroGenerado)
+            return numeroGenerado
+        }    
+}
+
+condicionesIniciales()
